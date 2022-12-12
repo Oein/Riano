ea099ff3cf8462c5ecd11b091eed80addb6b075d
Oein <oein0219@gmail.com>
2022-12-12 21:10:21 +0900

    white, black note

---
+++ src/blackNote.jsx
@@ -0,0 +1,63 @@
+import { midiHandler, mouseDown } from "./App";
+
+export default function BlackNote({ octave, pitch, enable }) {
+  return (
+    <div className="blackNote" id={`note.bc.${octave}.${pitch}`}>
+      <button
+        className="noteBtn"
+        id={`note.${octave}.${pitch}`}
+        onMouseDown={() => {
+          midiHandler({
+            type: "note_on",
+            key: (octave + 2) * 12 + pitch - 1,
+            vol: 88,
+          });
+        }}
+        onMouseUp={() => {
+          midiHandler({
+            type: "note_off",
+            key: (octave + 2) * 12 + pitch - 1,
+            vol: 88,
+          });
+        }}
+        onMouseLeave={() => {
+          midiHandler({
+            type: "note_off",
+            key: (octave + 2) * 12 + pitch - 1,
+            vol: 88,
+          });
+        }}
+        onMouseEnter={() => {
+          if (mouseDown)
+            midiHandler({
+              type: "note_on",
+              key: (octave + 2) * 12 + pitch - 1,
+              vol: 88,
+            });
+        }}
+      >
+        <div>
+          {enable === 2
+            ? pitch === 2
+              ? "W"
+              : pitch === 4
+              ? "E"
+              : pitch === 7
+              ? "T"
+              : pitch === 9
+              ? "Y"
+              : "U"
+            : enable === 1
+            ? pitch === 2
+              ? "O"
+              : pitch === 4
+              ? "P"
+              : pitch === 7
+              ? "]"
+              : null
+            : null}
+        </div>
+      </button>
+    </div>
+  );
+}
