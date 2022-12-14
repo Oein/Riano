030495ae02fc08e5b0fbfd45a2c60d371bae91fe
Oein <oein0219@gmail.com>
2022-12-14 22:58:18 +0900

    add toast

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
 import App from "./App.jsx";
 
 const rootElement = document.getElementById("root");
@@ -9,5 +12,6 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
+    <ToastContainer autoClose={3000} position={"bottom-right"} />
   </StrictMode>
 );
