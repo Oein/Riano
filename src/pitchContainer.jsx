b39f0c87896b2a74dbfeac2fb8308493f4d7692d
Oein <oein0219@gmail.com>
2022-12-14 19:11:00 +0900

    change calc formula

--- src/pitchContainer.jsx
+++ src/pitchContainer.jsx
@@ -1,13 +1,14 @@
 import { pitch } from "./keyboardInstu";
 
 export default function PitchContainer({ octave }) {
+  console.log(pitch - 2, octave - 1);
   return (
     <div className="pitcherContainer">
       <div
         className="pitchBar"
         style={{
-          marginLeft: `calc(100% / ${octave} * ${pitch - 2})`,
-          width: `calc(100% / ${octave} * 1.67)`,
+          marginLeft: `calc(100% * ${pitch - 3} / ${octave - 1})`,
+          width: `calc(100% / ${octave - 1} * 1.67)`,
         }}
       ></div>
     </div>
