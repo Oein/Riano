f42609e8c2bff9755035cf85cfb16eb7fdc56bd6
Oein <oein0219@gmail.com>
2022-12-15 21:20:28 +0900

    change it

--- src/inst/index.js
+++ src/inst/index.js
@@ -1,15 +1,7 @@
 import { Sampler } from "tone";
+import piano from "./piano.json";
 
-let inst = new Sampler({
-  urls: {
-    C4: "C4.mp3",
-    "D#4": "Ds4.mp3",
-    "F#4": "Fs4.mp3",
-    A4: "A4.mp3",
-  },
-  release: 1,
-  baseUrl: "https://tonejs.github.io/audio/salamander/",
-});
+let inst = new Sampler(piano);
 
 export default inst;
 
